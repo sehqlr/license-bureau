1b45c2e5ec64b5503bf5b7a5d3b3cff0aea4bc56
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-06 20:40:58 -0600

    Adding Dockerfile and rough draft script

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:latest
+MAINTAINER sehqlr
+
+
---
+++ license-bureau.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+"""
+Functions
+    Generating LICENSE file
+        Parse license.yml file in the root of the repo
+        create file, with sections for each license, and what projects use it,
+        or should it be other way around?
+    Generating license.yml file
+        Prompt the user for details to fill out file
+        create file based on prompts
+"""
